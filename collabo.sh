ebda9c9e5adebc9d730198e25f24d65b442b9644
parao0628 <parao0628@naver.com>
2018-07-25 13:02:24 +0900

    sh

--- collabo.sh
+++ collabo.sh
@@ -1,1 +1,1 @@
-python run.py --ncbi --jnlpba --bc5_chem --bc5_disease --bc4 --bc2 --num_class 5 --ce_dim 30 --clwe_dim 200 --clwe_method 'CNN' --batch_size 10 --hidden_size 300 --lr 0.01 --lr_pump --lr_decay 0.05 --embdropout 0.5 --lstmdropout 0.3 --epoch 30 --pretrained 201806211234
+python run.py --ncbi --jnlpba --bc5_chem --bc5_disease --bc4 --bc2 --num_class 5 --ce_dim 30 --clwe_dim 200 --clwe_method 'CNN' --batch_size 10 --hidden_size 300 --lr 0.01 --lr_pump --lr_decay 0.05 --embdropout 0.5 --lstmdropout 0.3 --pretrained STM_MODEL_DIRECTORY_NAME
