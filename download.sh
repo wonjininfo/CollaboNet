9972c74fd3e722f8ef128da57a63b5a5f0557178
chanho <parao0628@naver.com>
2018-11-01 12:17:36 +0900

    gdrive_wget

--- download.sh
+++ download.sh
@@ -7,8 +7,8 @@ python preprocessing.py
 
 mkdir wordvec
 cd wordvec
-wget https://s3-us-west-2.amazonaws.com/collabonet/wordvec/vocab.txt
-wget https://s3-us-west-2.amazonaws.com/collabonet/wordvec/pubmed_pmc_wiki_200dim_wordvec.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Q2TCwPRrEPtI75IHJ9awbKI5FH3lJS0w' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Q2TCwPRrEPtI75IHJ9awbKI5FH3lJS0w" -O pubmed_pmc_wiki_200dim_wordvec.txt && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1H1VUlvjeGjVGANqoadbn3Fx015JNE_nM' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1H1VUlvjeGjVGANqoadbn3Fx015JNE_nM" -O vocab.txt && rm -rf /tmp/cookies.txt
 cd ..
 
 mkdir modelSave
